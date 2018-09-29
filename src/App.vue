dc61f76da980e0a746d824aff0ba3ca72e0ebd32
Taylor Devereaux <taylordevereaux@live.ca>
2018-09-29 18:01:14 -0300

    feat: adds theme and transaction list progress

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <div id="wrapper">
+        <div id="sidebar" class="bg-dark">
+            <div id="logo_wrapper" class="px-4">
+                <img class="img-fluid" src="./assets/logo.png">
+            </div>
+            <TransactionsList />
+        </div>
+        <div id="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import TransactionsList from './components/TransactionsList.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    TransactionsList,
   },
 };
 </script>
 
 <style lang="scss">
+// importing the main theme.
+@import './assets/theme/scss/toolkit-inverse.scss';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
+
+#wrapper {
+  width: 100%;
+  height: 100%;
+  background-color: rgb(77,86,95);
+  overflow: hidden;
+}
+
+#wrapper > #sidebar {
+  height: 100%;
+  width: 300px;
+  top: 0;
+  bottom: 0;
+  position: fixed;
+  z-index: 10;
+  background-color: $gray-darkest;
+}
+
+#content {
+  height: 100%;
+  width: 100%;
+  margin-left: 300px;
+}
+
+#logo_wrapper {
+  display: inline-block;
+  border-bottom: 1px solid rgb(77,86,95);
+  height: 120px;
+}
+
 </style>
---
+++ src/components/TransactionsList.vue
@@ -0,0 +1,71 @@
+<template>
+<div id="transactions">
+  <ul>
+    <li v-for="item in transactions" :key="item.id">
+      <div class="list-item flex">
+        <span class="date">{{ item.date }}</span>
+        <span class="amount" v-bind:class="{ pos: item.amount>=0, neg: item.amount<0}">
+            {{ item.amount }}
+        </span>
+      </div>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionsList',
+  // props: {
+  //     msg: String,
+  // },
+  data() {
+    return {
+      transactions: [{
+        id: 1,
+        text: 'Paycheck',
+        amount: 344.34,
+        date: '2018-09-27',
+        repeats: 'biweekly',
+      }, {
+        id: 2,
+        text: 'Internet',
+        amount: -96.54,
+        date: '2018-09-13',
+        repeats: 'monthly',
+      }],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss" scoped>
+@import '../assets/theme/scss/variables-inverse.scss';
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+ul .list-item {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  margin: 10px;
+}
+
+.list-item .date {
+  font-weight: bold;
+}
+
+.list-item .amount.pos {
+  color: $brand-success;
+}
+
+.list-item .amount.neg {
+  color: $brand-danger;
+}
+</style>
